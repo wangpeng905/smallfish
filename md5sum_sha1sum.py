1a70fdf918b4326c3c0e3f1660b24ff431c1c9b0
wangpeng905 <wangpengbio@163.com>
2017-05-01 17:34:15 +0800

    Update md5sum_sha1sum.py

--- md5sum_sha1sum.py
+++ md5sum_sha1sum.py
@@ -8,26 +8,26 @@ import base64
 # Usage: python3 md5sum_sha1sum.py <inputfile> 
 
 def SHA1FileWithName(fineName, block_size=64 * 1024):
-  with open(fineName, 'rb') as f:
-    sha1 = hashlib.sha1()
-    while True:
-      data = f.read(block_size)
-      if not data:
-        break
-      sha1.update(data)
-    retsha1 = binascii.b2a_hex(sha1.digest())
-    return retsha1
+	with open(fineName, 'rb') as f:
+		sha1 = hashlib.sha1()
+		while True:
+			data = f.read(block_size)
+			if not data:
+				break
+			sha1.update(data)
+		retsha1 = binascii.b2a_hex(sha1.digest())
+	return retsha1
 
 def MD5FileWithName(fineName, block_size=64 * 1024):
-  with open(fineName, 'rb') as f:
-    md5 = hashlib.md5()
-    while True:
-      data = f.read(block_size)
-      if not data:
-        break
-      md5.update(data)
-    retmd5 = binascii.b2a_hex(md5.digest())
-    return retmd5
+	with open(fineName, 'rb') as f:
+		md5 = hashlib.md5()
+		while True:
+			data = f.read(block_size)
+			if not data:
+				break
+			md5.update(data)
+		retmd5 = binascii.b2a_hex(md5.digest())
+	return retmd5
 
 print("MD5:",MD5FileWithName(sys.argv[1]))
 print("SHA1:",SHA1FileWithName(sys.argv[1]))
